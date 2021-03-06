68b8a08226658a004ff74f8d2415efce0f854017
André Savioli <andre_savioli@hotmail.com>
2021-03-06 07:31:42 +0000

    Small adjustments

--- build-dev.sh
+++ build-dev.sh
@@ -1,18 +1,22 @@
 #!/bin/bash
 
-docker-compose -f ./britecore-backend/docker-compose.yaml up -d --build
-docker-compose -f ./britecore-frontend/docker-compose.yaml up -d --build
+docker-compose -f ./britecore-backend/docker-compose.yaml down
+docker-compose -f ./britecore-frontend/docker-compose.yaml down
+
+docker-compose -f ./britecore-backend/docker-compose.yaml build
+docker-compose -f ./britecore-frontend/docker-compose.yaml build
 
 docker-compose -f ./britecore-frontend/docker-compose.yaml run --rm vue sh -c "npm install"
 
 echo "Checking the connectivity with Postgres, please wait..."
 
-until docker container exec -it api pg_isready -U postgres -h postgres; do
+until docker-compose -f ./britecore-backend/docker-compose.yaml run --rm api pg_isready -U postgres -h postgres; do
   echo "Waiting for Postgres... "
   sleep 1
 done
 
 docker-compose -f ./britecore-backend/docker-compose.yaml run --rm api sh -c "python manage.py migrate"
+docker-compose -f ./britecore-backend/docker-compose.yaml run --rm api sh -c "python manage.py flush --no-input"
 docker-compose -f ./britecore-backend/docker-compose.yaml run --rm api sh -c "python manage.py loaddata risk/fixtures/risk_category.json"
 docker-compose -f ./britecore-backend/docker-compose.yaml run --rm api sh -c "python manage.py loaddata risk/fixtures/risk_field_type.json"
 docker-compose -f ./britecore-backend/docker-compose.yaml run --rm api sh -c "python manage.py loaddata risk/fixtures/risk_field.json"
@@ -20,3 +24,6 @@ docker-compose -f ./britecore-backend/docker-compose.yaml run --rm api sh -c "py
 docker-compose -f ./britecore-backend/docker-compose.yaml run --rm api sh -c "python manage.py loaddata risk/fixtures/risk_risk_field.json"
 docker-compose -f ./britecore-backend/docker-compose.yaml run --rm api sh -c "python manage.py loaddata risk/fixtures/risk_field_enum_option.json"
 docker-compose -f ./britecore-backend/docker-compose.yaml run --rm api sh -c "python manage.py loaddata risk/fixtures/risk_risk_field_risk_field_enum_option.json"
+
+docker-compose -f ./britecore-backend/docker-compose.yaml up -d
+docker-compose -f ./britecore-frontend/docker-compose.yaml up -d
