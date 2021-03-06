1de490f377f56f76a58c739d329f564572619583
André Savioli <andre_savioli@hotmail.com>
2021-03-06 19:27:06 +0000

    Removed several loaddata commands in favor of only the seed command

--- build-dev.sh
+++ build-dev.sh
@@ -17,13 +17,7 @@ done
 
 docker-compose -f ./britecore-backend/docker-compose.yaml run --rm api sh -c "python manage.py migrate"
 docker-compose -f ./britecore-backend/docker-compose.yaml run --rm api sh -c "python manage.py flush --no-input"
-docker-compose -f ./britecore-backend/docker-compose.yaml run --rm api sh -c "python manage.py loaddata risk/fixtures/risk_category.json"
-docker-compose -f ./britecore-backend/docker-compose.yaml run --rm api sh -c "python manage.py loaddata risk/fixtures/risk_field_type.json"
-docker-compose -f ./britecore-backend/docker-compose.yaml run --rm api sh -c "python manage.py loaddata risk/fixtures/risk_field.json"
-docker-compose -f ./britecore-backend/docker-compose.yaml run --rm api sh -c "python manage.py loaddata risk/fixtures/risk.json"
-docker-compose -f ./britecore-backend/docker-compose.yaml run --rm api sh -c "python manage.py loaddata risk/fixtures/risk_risk_field.json"
-docker-compose -f ./britecore-backend/docker-compose.yaml run --rm api sh -c "python manage.py loaddata risk/fixtures/risk_field_enum_option.json"
-docker-compose -f ./britecore-backend/docker-compose.yaml run --rm api sh -c "python manage.py loaddata risk/fixtures/risk_risk_field_risk_field_enum_option.json"
+docker-compose -f ./britecore-backend/docker-compose.yaml run --rm api sh -c "python manage.py seed"
 
 docker-compose -f ./britecore-backend/docker-compose.yaml up -d
 docker-compose -f ./britecore-frontend/docker-compose.yaml up -d
